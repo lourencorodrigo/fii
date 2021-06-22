788bdb99a4913dc9d34e8da07bca9eaa4824ba1b
Rodrigo Lourenço <rsilvape@gmail.com>
2021-06-22 10:31:29 -0300

    Adicionando dividendos dos últimos 12 meses

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const puppeteer = require("puppeteer");
-const readlineSync = require("readline-sync");
+const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
 const chalk = require('chalk');
 const ora = require('ora');
 
 const spinner = ora({
-	spinner: 'dots',
+  spinner: 'dots',
 });
 
 const log = (title, value) => {
@@ -23,14 +23,15 @@ const log = (title, value) => {
   // await page.screenshot({ path: "example.png" });
 
   const result = await page.evaluate(() => {
-    const indicators = document.getElementsByClassName("indicator-value");
-    const price = document.getElementsByClassName("price")[0].innerHTML.trim();
-    const title = document
-      .getElementsByClassName("MsoNormal")[0]
-      .getElementsByTagName("span")[0]
-      .innerText.trim();
+    const indicators = document.getElementsByClassName('indicator-value');
+    const price = document.getElementsByClassName('price')[0].innerHTML.trim();
+    const title = document.querySelector('.section-subtitle').innerText.trim();
+    const dividend = document.querySelector(
+      'table.table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(5)'
+    ).innerHTML;
 
     return {
+      dividend,
       title,
       price,
       liquidity: indicators[0].innerHTML.trim(),
@@ -48,6 +49,7 @@ const log = (title, value) => {
   log('Último Rendimento', result.lastYield);
   log('Dividend Yield', result.yield);
   log('P/VP', result.pvp);
+  log('Dividendos (12 meses)', result.dividend);
 
   await browser.close();
 })();
